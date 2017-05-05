a475c0cde79f13fb5036873b239918525e36d1e6
Pooya Parsa <pooya@pi0.ir>
2017-05-06 00:15:51 +0430

    Added PWA Helpers

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+const NuxtHelpers = require('nuxt-helpers');
+
+// https://github.com/fandogh/nuxt-helpers
+module.exports = NuxtHelpers(['optimize', 'offline', 'manifest'], {
   /*
   ** Headers of the page
   */
@@ -6,17 +9,20 @@ module.exports = {
     title: 'starter',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
     ]
   },
   /*
   ** Customize the progress-bar color
   */
   loading: { color: '#3B8070' },
+  /*
+  ** Customize manifest.json
+  */
+  manifest: {
+    name: 'Nuxt.js project',
+    theme_color: '#3B8070'
+  },
   /*
   ** Build configuration
   */
@@ -35,4 +41,4 @@ module.exports = {
       }
     }
   }
-}
+})
